2468cf33c3f283ca7ce7af5a068a7fb32aeaec97
wolog1 <1602328257@qq.com>
2020-02-22 18:37:21 +0800

    First

---
+++ sample.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: sample.c
+	> Author: Carlos
+	> Mail: zhy4246841@163.com
+	> Created Time: 2020年02月22日 星期六 18时35分08秒
+ ************************************************************************/
+
+#include<stdio.h>
+int main(int argc, char *argv[])
+{
+    printf("Hello world!\n");
+    return 0;
+
+}
