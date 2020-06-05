4b9481de025b23d3bb7d5fa082b51113b0d7a18a
1602328257@qq.com <@zhy-4246841>
2020-06-05 10:07:59 +0800

    第一次测试

---
+++ hello.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: hello.c
+	> Author: Carlos
+	> Mail: zhy4246841@163.com
+	> Created Time: 2020年06月05日 星期五 10时00分54秒
+ ************************************************************************/
+
+#include<stdio.h>
+int main(int argc, char *argv[])
+{
+    printf("Hello Linux!\n");
+    return 0;
+
+}
