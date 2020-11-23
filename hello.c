8a6db566fee4bd56bb60a75aa10a5261efd196aa
wolog1 <51894453+wolog1@users.noreply.github.com>
2020-11-23 16:18:59 +0800

    add 11.24
    test

--- hello.c
+++ hello.c
@@ -1,14 +1,8 @@
-/*************************************************************************
-	> File Name: hello.c
-	> Author: Carlos
-	> Mail: zhy4246841@163.com
-	> Created Time: 2020年06月05日 星期五 10时00分54秒
- ************************************************************************/
-
-#include<stdio.h>
-int main(int argc, char *argv[])
+#include <stdio.h>
+int main()
 {
-    printf("Hello Linux!\n");
-    return 0;
 
+	printf("hello qinqin!\n");
+	printf("hello Linux!\n");
+	return 0;
 }
